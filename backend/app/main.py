43f0ea58947f07914f60028e91e58daf4d22f434
ju <jubornin96@gmail.com>
2025-10-19 00:32:39 +0900

    Add CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 # CORS 설정 (프론트엔드와 통신 허용)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "https://tshirts-shop.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
